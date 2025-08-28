251878274f8fb609e18675f5e1ca1fc4ad08f10f
leonardo araujo <leokaique7@gmail.comm>
2025-08-28 17:44:11 -0300

    ajustes

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+bind = "127.0.0.1:8000"
+workers = 4
+worker_class = "sync"
+worker_connections = 1000
+timeout = 120
+keepalive = 300
+preload_app = True
+max_requests = 1000
+max_requests_jitter = 100
