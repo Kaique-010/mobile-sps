653db47cbeee9e7a884ef650d4bf329e2dd16416
Leo <leokaique7@gmail.com>
2026-03-04 10:50:03 -0300

    ordens finalizadas no back

--- OrdemdeServico/pagination.py
+++ OrdemdeServico/pagination.py
@@ -3,7 +3,7 @@ from rest_framework.pagination import LimitOffsetPagination
 
 class OrdemServicoPagination(LimitOffsetPagination):
     """Paginação customizada para Ordens de Serviço"""
-    default_limit = 350    
+    default_limit = 100    
     limit_query_param = 'limit'
     offset_query_param = 'offset'
-    max_limit = 350     # Limite máximo
+    max_limit = 100     # Limite máximo
